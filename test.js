6659ff9feec236d84f07792002e79606bbe1169c
zerius182 <garryjoynes182@hotmail.co.uk>
2022-05-11 13:53:01 +0100

    Added new stuff to test

--- test.js
+++ test.js
@@ -5,3 +5,7 @@ testPlayer.getPlayerName();
 testPlayer.getWinCount();
 testPlayer.victory();
 testPlayer.getWinCount();
+testPlayer.setActive();
+testPlayer.showActiveStatus();
+testPlayer.setWaiting();
+testPlayer.showActiveStatus();
