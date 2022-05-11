711acd0705f98af6532ac01324424a1b8bec1bd3
zerius182 <garryjoynes182@hotmail.co.uk>
2022-05-11 12:18:19 +0100

    Added console logs to factory methods

--- game_script.js
+++ game_script.js
@@ -1,21 +1,33 @@
-const playerFactory = marker =>{
+const playerFactory = (marker, playerName) =>{
+
     let currentScore = 0;
     let playerMarker = marker;
+    let _playerName = playerName;
+
     const getWinCount = () =>{
+        console.log(`${_playerName} currently has ${currentScore} points`)
         return currentScore;
     }
 
     const victory = () =>{
+        console.log(`${_playerName} has just won a round!`)
         return currentScore ++;
     }
 
     const getMarker = () =>{
+        console.log(`${_playerName} is using ${playerMarker}`)
         return playerMarker;
+    }
+
+    const getPlayerName = () =>{
+        console.log(`This player is named ${_playerName}`)
+        return _playerName;
     }
 
     return{
         getWinCount,
         victory,
-        getMarker
+        getMarker,
+        getPlayerName
     }
 }
