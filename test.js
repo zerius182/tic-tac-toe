b61691d1a8142703ce231acc59e633b9b572e189
zerius182 <garryjoynes182@hotmail.co.uk>
2022-05-13 14:09:22 +0100

    Saved winning combos to test.js

--- test.js
+++ test.js
@@ -9,3 +9,7 @@ testPlayer.setActive();
 testPlayer.showActiveStatus();
 testPlayer.setWaiting();
 testPlayer.showActiveStatus();
+
+winningCombos = [
+    [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
+]
